e2ed181a24f3c6597787126f8c082b4a9fa9f399
MBA Innovation <montassar.benabdallah@gmail.com>
2026-01-26 16:03:14 -0500

    Fix site URL to www.horizontechmba.com in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://horizontechmba.com',
+  site: 'https://www.horizontechmba.com',
   integrations: [tailwind()],
   i18n: {
     defaultLocale: 'en',
